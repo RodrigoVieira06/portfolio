6652de25cf204988ee84249fe983d960a59b9001
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2023-11-02 21:17:18 -0300

    feat: developed the header and footer components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,30 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5C8FA4',
+        primaryDark: '#273E47',
+        primaryLight: '#8AD4F1',
+
+        secundary: '#0F1020',
+        secundaryDark: '#494D9C',
+        secundaryLight: '#494D9C',
+
+        bkgPrimary: '#FEFCFD',
+        bkgSecundary: '#F5F5F5',
+      },
+      dark: {
+        'bg-dark': '#000505',
+        'secundaryDark': '#000505',
+        'backgroundDark': '#000505'
+      },
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class'
 }
 
