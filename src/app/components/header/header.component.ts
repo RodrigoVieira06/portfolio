202d06f0dd5b91535df300655fda0fba332585d4
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2023-11-17 01:03:02 -0300

    feat: changed the home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
 
   public pathName: string = '';
+  public linkedinLink: string = 'https://www.linkedin.com/in/rodrigovl12/';
+  public githubLink: string = 'https://github.com/RodrigoVieira06';
+  public whastappLink: string = 'https://wa.me/5521990012455';
 
   constructor(private router: Router) {
     this.pathName = this.router.url;
