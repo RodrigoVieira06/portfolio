2003025239c00e993cd12dea6c8bd3dfac651ae8
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2024-05-28 14:27:00 -0300

    feat: added basic content to the project

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  public pathName: string = '';
+  public linkedinLink: string = 'https://www.linkedin.com/in/rodrigovl12/';
+  public githubLink: string = 'https://github.com/RodrigoVieira06';
+  public whastappLink: string = 'https://wa.me/5521990012455';
 
+  constructor(private router: Router) {
+    this.pathName = this.router.url;
+  }
 }
