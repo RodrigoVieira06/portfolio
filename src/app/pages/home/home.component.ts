26fd23949aef9bb3a8d71c9fd2b7ba9289209382
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2024-05-29 02:46:36 -0300

    feat: developed some stylizations on the home screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @ViewChild('popup', { static: true }) popup!: ElementRef<HTMLDivElement>;
 
+  onMouseMove(event: MouseEvent): void {
+    const imageContainer = event.currentTarget as HTMLElement;
+    const rect = imageContainer.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    this.popup.nativeElement.style.left = `${mouseX}px`;
+    this.popup.nativeElement.style.top = `${mouseY}px`;
+    this.popup.nativeElement.style.opacity = '1';
+  }
+
+  onMouseLeave(): void {
+    this.popup.nativeElement.style.opacity = '0';
+  }
 }
