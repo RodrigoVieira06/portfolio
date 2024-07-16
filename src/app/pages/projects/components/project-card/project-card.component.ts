0c448c6a1a08fde17818597ea937e64b8e29d0e4
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2024-07-15 22:18:59 -0300

    improvement: adding esc function in the modal component

--- src/app/pages/projects/components/project-card/project-card.component.ts
+++ src/app/pages/projects/components/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { IProject } from 'src/app/shared/types/project.type';
 
 @Component({
@@ -6,11 +6,23 @@ import { IProject } from 'src/app/shared/types/project.type';
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent {
-
+export class ProjectCardComponent implements OnInit, OnDestroy {
   @Input() public entity!: IProject;
-
   public showModal: boolean = false;
+
+  ngOnInit(): void {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  public handleKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Escape' && this.showModal) {
+      this.toggleModal();
+    }
+  }
 
   public toggleModal(): void {
     this.showModal = !this.showModal;
